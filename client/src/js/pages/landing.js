e13ea844c3ad9dbec59e300e4f1f1f35a2b81060
rsiddik7 <rsiddik369@gmail.com>
2025-04-29 07:08:14 +0700

    add hero section

--- client/src/js/pages/landing.js
+++ client/src/js/pages/landing.js
@@ -1,3 +1,5 @@
+import heroImage from '../../assets/img/work-team-art.jpg';
+
 export default function LandingPage() {
   return `
       <header class="header">
@@ -10,6 +12,32 @@ export default function LandingPage() {
         </nav>
       </header>
 
+      <section class="hero">
+        <div class="hero__content">
+          <div class="hero__content-info">
+            <div class="hero__content-up">
+              <h3>Simple Notes Planning Tools</h3>
+            </div>
+            
+            <div class="hero__content-term">
+              <h2><span class="create">Create.</span> Organize.</h2>
+              <h2>Clean. <span class="easy">Easy.</span></h2>
+              <p>Capture your ideas effortlessly. Stay focused and keep everything organized in one place with Memoa Notes.</p>
+            </div>
+
+            <div class="hero__button">
+              <a href="#/login" class="started-btn">Get Started</a>
+            </div>
+          </div>
+
+          <div class="hero__image">
+            <img src="${heroImage}" alt="work-team-art">
+          </div>
+        </div>
+      </section>
+
+      <section class="about">
       
+      </section>
     `;
 }
